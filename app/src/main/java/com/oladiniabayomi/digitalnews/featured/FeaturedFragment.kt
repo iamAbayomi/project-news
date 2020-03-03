f55f0e5d118aadfcb8b85a189241be269aa4616b
iamabayomi <Oladinifolawiyo@gmail.com>
2020-03-03 12:19:25 +0100

    Addded Featured Fragments

---
+++ app/src/main/java/com/oladiniabayomi/digitalnews/featured/FeaturedFragment.kt
@@ -0,0 +1,66 @@
+package com.oladiniabayomi.digitalnews.featured
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+import com.oladiniabayomi.digitalnews.R
+import kotlinx.android.synthetic.main.activity_detailed_article.*
+import kotlinx.android.synthetic.main.fragment_featured.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FeaturedFragment : Fragment() {
+
+    val arg : String = "param1"
+    val args2: String = "param2"
+
+    var param1 : String? = null
+    var param2 : String? = null
+
+
+
+    fun newInstance( imageUrl : String, title: String ) :FeaturedFragment {
+        val fragment = FeaturedFragment ()
+        val args = Bundle()
+        args.putString(arg, imageUrl)
+        args.putString(args2, title)
+        fragment.arguments = args
+        return  fragment
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null){
+            param1 = arguments!!.getString(arg)
+            param2 = arguments!!.getString(args2)
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var view  = inflater.inflate(R.layout.fragment_featured, container, false)
+
+        var featuredText = view.findViewById<TextView>(R.id.featured_text)
+        var featuredImage = view.findViewById<ImageView>(R.id.featured_image)
+
+        featuredText.text = param2
+
+        Glide.with(this)
+            .load(param1)
+            .into(featuredImage)
+
+
+        return view
+    }
+
+}
--- app/src/main/java/com/oladiniabayomi/digitalnews/home/HomeFragment.kt
+++ app/src/main/java/com/oladiniabayomi/digitalnews/home/HomeFragment.kt
@@ -5,16 +5,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import androidx.viewpager.widget.ViewPager
 import com.oladiniabayomi.digitalarticles.articles.Articles
+import com.oladiniabayomi.digitalnews.MyPageIndicator
 import com.oladiniabayomi.digitalnews.R
 import com.oladiniabayomi.digitalnews.articles.ArticlesRecyclerViewAdapter
 import com.oladiniabayomi.digitalnews.detailed_article.DetailedArticleActivity
+import com.oladiniabayomi.digitalnews.featured.FeaturedFragment
 import com.oladiniabayomi.digitalnews.interfaces.OnItemClickListener
 import com.oladiniabayomi.digitalnews.network.PostsService
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -25,75 +31,123 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class HomeFragment : Fragment(), OnItemClickListener  {
-
+class HomeFragment : Fragment(), OnItemClickListener {
 
     private lateinit var homeViewModel: HomeViewModel
     //RecyclerView Variables
     lateinit var recyclerView: RecyclerView
-    private var  articlesRecyclerViewAdapter : ArticlesRecyclerViewAdapter? = null
+    private var articlesRecyclerViewAdapter: ArticlesRecyclerViewAdapter? = null
     private var layoutManager: LayoutManager? = null
+    private var currentArticles = ArrayList<Articles>()
 
-    private var currentArticles  = ArrayList<Articles>()
+    //ViewPager variables
+    var mViewPager: ViewPager? = null;
+    var mLinearLayout: LinearLayout? = null
+    var mAdapter: CustomPagerAdapter2? = null
+    var mIndicator : MyPageIndicator? = null
+    var fragments = ArrayList<Fragment>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
 
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+        mAdapter = CustomPagerAdapter2(activity!!.supportFragmentManager,fragments)
+
+        mViewPager = root.findViewById(R.id.viewPager)
+
+        mLinearLayout = root.findViewById(R.id.pagesContainer)
+
+
+
+        //RecyclerView implementation
         recyclerView = root.findViewById(R.id.home_recyclerview)
         layoutManager = LinearLayoutManager(activity)
-
         articlesRecyclerViewAdapter =
-            context?.let { ArticlesRecyclerViewAdapter(it, currentArticles , this) }
-
-
+            context?.let { ArticlesRecyclerViewAdapter(it, currentArticles, this) }
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter =  articlesRecyclerViewAdapter
+        recyclerView.adapter = articlesRecyclerViewAdapter
 
+        for ( x in 0..5 ){
+            fragments.add(FeaturedFragment().newInstance("https://i2.wp.com/www.tell.com.ng/wp-content/uploads/2020/02/images-1-1.jpeg?fit=610%2C503&ssl=1",
+                "Loading"))
+        }
+
+
+        //Call for the retrofit class
         getCurrentData()
 
+        mViewPager!!.adapter= mAdapter
+
+
+        mIndicator = MyPageIndicator(activity!!.applicationContext, mLinearLayout!!, mViewPager!! , R.drawable.tab_selector)
+
+        mIndicator!!.setPageCount(fragments.size)
+        mIndicator!!.show()
 
         return root
     }
 
+
     override fun onItemClick(articles: Articles) {
-        val intent = Intent( activity , DetailedArticleActivity::class.java)
-        intent.putExtra("articles", articles  )
+        val intent = Intent(activity, DetailedArticleActivity::class.java)
+        intent.putExtra("articles", articles)
         startActivity(intent)
 
     }
 
     private fun getCurrentData() {
-        val retrofit =  Retrofit.Builder()
+        val retrofit = Retrofit.Builder()
             .baseUrl("http://www.tell.com.ng/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
         val service = retrofit.create(PostsService::class.java)
         val call = service.getPosts()
 
-        call.enqueue( object : Callback<List<Articles>> {
-            override fun onResponse(call: Call<List<Articles>>?, response: Response<List<Articles>>?) {
-                if(response!!.code() == 200){
-
+        call.enqueue(object : Callback<List<Articles>> {
+            override fun onResponse(
+                call: Call<List<Articles>>?,
+                response: Response<List<Articles>>?
+            ) {
+                if (response!!.code() == 200) {
                     currentArticles.addAll(response.body())
 
+                    fragments.clear()
+
+                    for ( x in 0..5 ){
+                        fragments.add(FeaturedFragment().newInstance(currentArticles[x].articlesThumbnailImage!!,
+                            currentArticles[x].articlesTitle!!.rendered!!))
+                    }
+
+                    mAdapter!!.notifyDataSetChanged()
                     articlesRecyclerViewAdapter!!.notifyDataSetChanged()
                 }
             }
 
             override fun onFailure(call: Call<List<Articles>>?, t: Throwable?) {
-             //   textView!!.text = t!!.message
+                //   textView!!.text = t!!.message
             }
-
-
         })
-
     }
+
+
+     class CustomPagerAdapter2(fm: FragmentManager, frags: List<Fragment>) : FragmentStatePagerAdapter(fm) {
+
+        var mFrags : List<Fragment> = frags
+
+         override fun getItem(position: Int): Fragment {
+           var index: Int = position % mFrags.size
+
+           return mFrags.get(index)
+         }
+
+         override fun getCount(): Int {
+            return Integer.MAX_VALUE
+          }
+
+     }
 }
