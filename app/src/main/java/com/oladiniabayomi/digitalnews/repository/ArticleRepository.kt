065e130d4d75fca25d0093cf2c58ddb62dccfa17
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-03-05 05:05:20 +0100

    Refactored Webservice

--- app/src/main/java/com/oladiniabayomi/digitalnews/repository/ArticleRepository.kt
+++ app/src/main/java/com/oladiniabayomi/digitalnews/repository/ArticleRepository.kt
@@ -15,33 +15,32 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.Executor
 
 // Declares the DAO as a private property in the constructor. Pass in the DAO
 // instead of the whole database, because you only need access to the DAO
 class ArticleRepository( private val articlesDao: ArticlesDao, var context: Application){
 
-    // private val webSer
+    var isInitialize = false
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
     //val allArticles : LiveData<List<Articles>> = articlesDao.getAllArticles()
-    val allArticles : LiveData<List<Articles>> = getArticles()
-
+     val allArticles : LiveData<List<Articles>> = getArticles()
     val FRESH_TIMEOUT_IN_MINUTES = 2
+
+    //private val executor=  Executor()
 
     suspend fun insert(articles: Articles){
         articlesDao.insertArticles(articles)
     }
 
-    fun getArticles() : LiveData<List<Articles>>{
-
+    suspend fun getArticles() : LiveData<List<Articles>>{
         return  refreshArticles()
     }
 
-
-
-     fun refreshArticles() : LiveData<List<Articles>> {
-
+     //Call for Retrofit
+   suspend  fun refreshArticles() : LiveData<List<Articles>> {
         val retrofit = Retrofit.Builder()
             .baseUrl("http://www.tell.com.ng/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -49,21 +48,26 @@ class ArticleRepository( private val articlesDao: ArticlesDao, var context: Appl
         val service = retrofit.create(PostsService::class.java)
 
         val call = service.getPosts()
-        val data = MutableLiveData<List<Articles>>()
+        var data = MutableLiveData<List<Articles>>()
 
+         if (!isInitialize){
         call.enqueue(object : Callback<List<Articles>> {
             override fun onResponse(call: Call<List<Articles>>?, response: Response<List<Articles>>?) {
                 if (response!!.code() == 200)
                 {
                     data.value = response.body()
                     Toast.makeText(context, response.body()[1].toString(), Toast.LENGTH_LONG).show()
-
+                    articlesDao.insertArticles(response.body()[1])
+                    isInitialize = true
                 }
              }
             override fun onFailure(call: Call<List<Articles>>?, t: Throwable?) {
-
             }
         })
+        }else{
+             data = articlesDao.getAllArticles() as MutableLiveData<List<Articles>>
+         }
+
         return  data
     }
 }
