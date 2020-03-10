93c678e23f16ff2fb970b2c5eee811eb59f7d41f
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-03-10 04:23:02 +0100

    Created Adapter for RecyclerView

--- app/src/main/java/com/oladiniabayomi/digitalnews/saved_articles/SavedArticlesFragment.kt
+++ app/src/main/java/com/oladiniabayomi/digitalnews/saved_articles/SavedArticlesFragment.kt
@@ -5,12 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.oladiniabayomi.digitalarticles.articles.Articles
+import com.oladiniabayomi.digitalarticles.articles.SavedArticles
 import com.oladiniabayomi.digitalnews.R
+import com.oladiniabayomi.digitalnews.articles.ArticlesRecyclerViewAdapter
+import com.oladiniabayomi.digitalnews.interfaces.SavedOnItemClickListener
 
-class SavedArticlesFragment : Fragment() {
+class SavedArticlesFragment : Fragment(), SavedOnItemClickListener {
+
 
     private lateinit var savedArticlesViewModel: SavedArticlesViewModel
+
+    lateinit var recyclerView: RecyclerView
+    private var articlesRecyclerViewAdapter: SavedRecyclerViewAdapter? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var currentArticles = ArrayList<SavedArticles>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,9 +31,24 @@ class SavedArticlesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         savedArticlesViewModel =
-            ViewModelProviders.of(this).get(SavedArticlesViewModel::class.java)
+            ViewModelProvider(this).get(SavedArticlesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_saved, container, false)
+        recyclerView = root.findViewById(R.id.saved_recyclerView)
+        layoutManager = LinearLayoutManager(activity)
+
+        articlesRecyclerViewAdapter =
+            context?.let { SavedRecyclerViewAdapter(it, currentArticles, this) }
+        //setting Recycler Views attribute
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = articlesRecyclerViewAdapter
+
+
 
         return root
     }
+
+    override fun onItemClick(articles: SavedArticles) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
--- app/src/main/java/com/oladiniabayomi/digitalnews/saved_articles/SavedArticlesRecyclerViewAdapter.kt
+++ app/src/main/java/com/oladiniabayomi/digitalnews/saved_articles/SavedArticlesRecyclerViewAdapter.kt
@@ -1,24 +1,66 @@
 package com.oladiniabayomi.digitalnews.saved_articles
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.oladiniabayomi.digitalarticles.articles.Articles
+import com.oladiniabayomi.digitalarticles.articles.SavedArticles
+import com.oladiniabayomi.digitalnews.R
+import com.oladiniabayomi.digitalnews.articles.ArticlesRecyclerViewHolder
+import com.oladiniabayomi.digitalnews.interfaces.OnItemClickListener
+import com.oladiniabayomi.digitalnews.interfaces.SavedOnItemClickListener
 
-class SavedRecyclerViewAdapter : RecyclerView.Adapter<SavedRecyclerViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedRecyclerViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+class SavedRecyclerViewAdapter(var context: Context, var currentArticles: ArrayList<SavedArticles>?, val listener: SavedOnItemClickListener) : RecyclerView.Adapter<SavedRecyclerViewHolder>() {
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        private val mInflater: LayoutInflater = LayoutInflater.from(context)
 
-    override fun onBindViewHolder(holder: SavedRecyclerViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        internal fun setArticles(currentArticles: ArrayList<SavedArticles>?){
+            this.currentArticles = currentArticles
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedRecyclerViewHolder {
+            val mItemView : View = mInflater.inflate(R.layout.article_item_view, parent, false)
+            return  SavedRecyclerViewHolder(mItemView)
+
+        }
+
+        override fun getItemCount(): Int {
+            return currentArticles!!.size
+        }
+
+        override fun onBindViewHolder(holder:SavedRecyclerViewHolder, position: Int) {
+            var currentArticles :SavedArticles = currentArticles!![position]
+            holder.bindTo(currentArticles, context, listener)
+        }
     }
-}
 
 class SavedRecyclerViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
 
+    val articleImage : ImageView =  itemView.findViewById(R.id.article_image)
+    val articleTitle : TextView = itemView.findViewById(R.id.article_title)
+    val articleDate : TextView = itemView.findViewById(R.id.article_date)
 
+
+
+    fun bindTo(currentArticles:SavedArticles, context: Context, listener: SavedOnItemClickListener) {
+        //   articleImage.setImageResource()
+
+        Glide.with(context)
+            .load(currentArticles.articlesThumbnailImage)
+            .into(articleImage)
+
+        articleTitle.text = currentArticles.articlesTitle!!.rendered
+        articleDate.text = currentArticles.articlesTimeStamp
+
+        itemView.setOnClickListener{
+            listener.onItemClick( currentArticles)
+        }
+    }
 }
