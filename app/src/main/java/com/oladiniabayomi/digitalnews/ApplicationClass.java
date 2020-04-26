8ab964886286e175e195a44aa3c92491996a405a
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-04-26 23:33:26 +0100

    Added OneSignal SDK

--- app/src/main/java/com/oladiniabayomi/digitalnews/ApplicationClass.java
+++ app/src/main/java/com/oladiniabayomi/digitalnews/ApplicationClass.java
@@ -4,12 +4,21 @@ import android.app.Application;
 
 import com.adjust.sdk.Adjust;
 import com.adjust.sdk.AdjustConfig;
+import com.onesignal.OneSignal;
 
 public class ApplicationClass extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
+
         String appToken = "aorsgbhwgm4g";
         String environment = AdjustConfig.ENVIRONMENT_SANDBOX;
         AdjustConfig config = new AdjustConfig(this, appToken, environment);
